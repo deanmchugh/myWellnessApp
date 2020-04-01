d3fa44bb446273be9ae44504c17a8778740d7b9b
Dean McHugh <dean.mchugh1988@gmail.com>
2020-04-01 16:22:06 +1000

    popup

---
+++ src/components/tasks/Header.tsx
@@ -0,0 +1,7 @@
+import React, {useState} from 'react';
+import {SafeAreaView, Text} from 'react-native';
+import {Styles} from '../../styles/Styles';
+
+export const Header = () => {
+  const [showPopup, setShowPopup] = useState(false);
+};
