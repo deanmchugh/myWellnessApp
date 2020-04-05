6888c5898d7a45d210c4c3f2451e4426f7be474d
Dean McHugh <dean.mchugh1988@gmail.com>
2020-04-05 17:37:26 +1000

    list item

--- src/components/tasks/ListViewItem.tsx
+++ src/components/tasks/ListViewItem.tsx
@@ -1,68 +1,42 @@
-import React, {Component} from 'react';
-import {TouchableHighlight, View, Text} from 'react-native';
-import CheckBox from './CheckBox';
-import TodoService from './TodoService';
+import React, {useState, useEffect} from 'react';
+import {TouchableHighlight, View, Text, StyleSheet} from 'react-native';
+import {TaskService} from '../../database/Services';
 
-class ListViewItem extends Component {
-  constructor(props) {
-    super(props);
-    this._onCheckBoxPressed = this._onCheckBoxPressed.bind(this);
-    this.state = {
-      data: this.props.data,
-    };
-  }
+export const ListViewItem = (props: any) => {
+  const [data, setData] = useState(props.data);
 
-  componentWillReceiveProps(props) {
-    this.setState({
-      data: props.data,
-    });
-  }
+  useEffect(() => {
+    setData(props.data);
+  }, [props.data]);
 
-  _onCheckBoxPressed() {
-    var data = this.state.data;
-    TodoService.update(data, () => {
-      data.completed = !data.completed;
-    });
-    this.setState({
-      data: data,
-    });
-
-    this.props.onCompletedChange();
-  }
-
-  render() {
-    let data = this.state.data;
-    let color = data.completed ? '#C5C8C9' : '#000';
-    let textDecorationLine = data.completed ? 'line-through' : 'none';
-    return (
-      <TouchableHighlight
-        underlayColor={'#eee'}
-        style={{
-          paddingTop: 6,
-          paddingBottom: 6,
-          backgroundColor: '#F8F8F8',
-          borderBottomWidth: 1,
-          borderColor: '#eee',
-        }}
-        {...this.props.sortHandlers}>
-        <View style={{flexDirection: 'row', alignItems: 'center'}}>
-          <CheckBox
-            data={data}
-            color={color}
-            onCheckBoxPressed={this._onCheckBoxPressed}
-          />
-          <Text
-            style={{
-              fontSize: 18,
-              color: color,
-              textDecorationLine: textDecorationLine,
-            }}>
-            {data.title}
-          </Text>
-        </View>
+  return (
+    <TouchableHighlight
+      underlayColor={'#eee'}
+      style={styles.container}
+      {...props.sortHandlers}>
+      <View style={styles.textCenter}>
+        <Text style={styles.title}>{data.title}</Text>
+      </View>
+      <TouchableHighlight style={styles.textCenter}>
+        <Text style={styles.title}>EDIT</Text>
       </TouchableHighlight>
-    );
-  }
-}
+    </TouchableHighlight>
+  );
+};
 
-module.exports = ListViewItem;
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 6,
+    paddingBottom: 6,
+    backgroundColor: '#F8F8F8',
+    borderBottomWidth: 1,
+    borderColor: '#eee',
+  },
+  textCenter: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 18,
+  },
+});
